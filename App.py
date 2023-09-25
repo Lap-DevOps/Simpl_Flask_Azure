331d9e595110008ac6517657c08d1ddbad0c2076
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2023-09-25 18:25:30 +0100

    init

--- App.py
+++ App.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template, current_app
+import os
+
+from flask import Flask, render_template, current_app, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    flask_ins= current_app
-    return render_template('home.html', current_app=flask_ins)
+    flask_ins = current_app
+    server_info = {
+        'Server Host': request.host,
+        'Server URL Root': request.url_root,
+        'Environment': app.config.get('ENV'),
+        'server_software': os.environ.get('SERVER_SOFTWARE'),
+        'port': app.config.get('PORT'),
+        'root_path': app.config.get('root_path'),
+        'DEBUG': app.config.get('DEBUG'),
+        'TESTING': app.config.get('TESTING'),
+        'WERKZEUG_RUN_MAIN': os.environ.get('WERKZEUG_RUN_MAIN'),
+        'ROUTS': app.url_map,
+        'wsgi - SERVER_SOFTWARE ': request.environ['SERVER_SOFTWARE'],
+        'wsgi - multithread ': request.environ['wsgi.multithread'],
+        'wsgi - multiprocess ': request.environ['wsgi.multiprocess'],
+        'wsgi - REMOTE_ADDR': request.environ['REMOTE_ADDR'],
+        'wsgi - REMOTE_PORT': request.environ['REMOTE_PORT'],
+        'wsgi - SERVER_NAME': request.environ['SERVER_NAME'],
+        'wsgi - SERVER_PORT': request.environ['SERVER_PORT'],
+        'wsgi - HTTP_HOST': request.environ['HTTP_HOST'],
+
+    }
+    return render_template('home.html', current_app=flask_ins, server_info=server_info)
 
 
 if __name__ == '__main__':
