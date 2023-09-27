d31e3d15bf1ec45c3a98b957c8d0bdd8aebc68d3
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2023-09-27 12:36:49 +0100

    init

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,15 +2,11 @@ from flask import Flask
 
 from config import Config
 
-def create_app():
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    app = Flask(__name__)
-    app.config.from_object(Config)
+app.config.from_envvar('FLASK_ENV', silent=True)
 
-    app.config.from_envvar('FLASK_ENV', silent=True)
+from blog.main.routes import main
 
-    from blog.main.routes import main
-
-    app.register_blueprint(main)
-
-    return app
+app.register_blueprint(main)
